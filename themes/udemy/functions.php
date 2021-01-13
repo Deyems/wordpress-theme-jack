7f85e9c69381546b620c678c2f9f712c1cf1d6e9
deyems <adeyemisola11@gmail.com>
2021-01-13 21:08:41 +0100

    loadiing and including hook for adding menu to admin dashboard

--- themes/udemy/functions.php
+++ themes/udemy/functions.php
@@ -5,7 +5,10 @@ define('JU_DEV_MODE', true);
 
 //Includes
 include (get_theme_file_path('/includes/front/enqueue.php'));
+include (get_theme_file_path('/includes/setup.php'));
 
 //Hooks
 add_action('wp_enqueue_scripts', 'ju_enqueue');
+add_action('after_setup_theme', 'ju_setup_theme');
+
 //Shortcodes
