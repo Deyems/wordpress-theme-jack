6e0e6fe1bea881bb03be908d1ea3d7aecd5abd74
deyems <adeyemisola11@gmail.com>
2021-01-13 17:14:04 +0100

    updating 4th option for wp_register_* to handle caching

--- themes/udemy/includes/front/enqueue.php
+++ themes/udemy/includes/front/enqueue.php
@@ -1,15 +1,20 @@
 <?php
 function ju_enqueue(){
     $uri = get_theme_file_uri();
-    wp_register_style('ju_google_fonts', 'https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i');
-    wp_register_style('ju_bootstrap', $uri. '/assets/css/bootstrap.css');
-    wp_register_style('ju_style', $uri. '/assets/css/style.css');
-    wp_register_style('ju_dark', $uri. '/assets/css/dark.css');
-    wp_register_style('ju_font-icons', $uri. '/assets/css/font-icons.css');
-    wp_register_style('ju_magnific-popup', $uri. '/assets/css/magnific-popup.css');
-    wp_register_style('ju_animate', $uri. '/assets/css/animate.css');
-    wp_register_style('ju_custom', $uri. '/assets/css/custom.css');
-    wp_register_style('ju_responsive', $uri. '/assets/css/responsive.css');
+    $ver = JU_DEV_MODE ? time() : false;
+    wp_register_style('ju_google_fonts', 
+    'https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Raleway:300,400,500,600,700|Crete+Round:400i',
+    [],
+    $ver
+    );
+    wp_register_style('ju_bootstrap', $uri. '/assets/css/bootstrap.css', [], $ver);
+    wp_register_style('ju_style', $uri. '/assets/css/style.css', [], $ver);
+    wp_register_style('ju_dark', $uri. '/assets/css/dark.css', [], $ver);
+    wp_register_style('ju_font-icons', $uri. '/assets/css/font-icons.css', [], $ver);
+    wp_register_style('ju_magnific-popup', $uri. '/assets/css/magnific-popup.css', [], $ver);
+    wp_register_style('ju_animate', $uri. '/assets/css/animate.css', [], $ver);
+    wp_register_style('ju_custom', $uri. '/assets/css/custom.css', [], $ver);
+    wp_register_style('ju_responsive', $uri. '/assets/css/responsive.css', [], $ver);
 
     wp_enqueue_style('ju_google_fonts');
     wp_enqueue_style('ju_bootstrap');
@@ -21,8 +26,8 @@ function ju_enqueue(){
     wp_enqueue_style('ju_custom');
     wp_enqueue_style('ju_responsive');
 
-    wp_register_script('ju_plugin', $uri. '/assets/js/plugins.js', [], false, true);
-    wp_register_script('ju_functions', $uri. '/assets/js/functions.js', [], false, true);
+    wp_register_script('ju_plugin', $uri. '/assets/js/plugins.js', [], $ver, true);
+    wp_register_script('ju_functions', $uri. '/assets/js/functions.js', [], $ver, true);
 
     wp_enqueue_script('jquery');
     wp_enqueue_script('ju_plugin');
