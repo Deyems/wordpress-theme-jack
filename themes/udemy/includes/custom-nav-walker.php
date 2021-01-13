faa1df1790968458f1e6a2a4616e9fba3237b0c1
deyems <adeyemisola11@gmail.com>
2021-01-13 22:43:48 +0100

    extend the Walker class and make custom one

--- themes/udemy/includes/custom-nav-walker.php
+++ themes/udemy/includes/custom-nav-walker.php
@@ -1,5 +1,23 @@
 <?php
 
 class JU_Custom_Nav_Walker extends Walker_Nav_Menu{
-    
+    public function start_lvl( &$output, $depth = 0, $args = [] ){
+        $output .= '<ul class="special-class">';
+    }
+
+    public function start_el( &$output, $item, $depth = 0, $args = [], $id = 0 ){
+        $output .= '<li class="special-class-item">';
+        $output .= '<a href="'. $item->url . '">';
+        $output .= $args->link_before . $item->title . $args->link_after;
+        $output .= '</a>';
+        $output .= $args->after;
+    }
+
+    public function end_el( &$output, $item, $depth = 0, $args = [], $id = 0 ){
+        $output .= '</li>';
+    }
+
+    public function end_lvl( &$output, $depth = 0, $args = [] ){
+        $output .= '</ul>';
+    }
 }
