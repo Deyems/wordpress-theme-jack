f4f43db1a459324cd32084a17f702919e9696c72
deyems <adeyemisola11@gmail.com>
2021-01-13 23:33:32 +0100

    carved out of index for footer section

---
+++ themes/udemy/footer.php
@@ -0,0 +1,54 @@
+    <!-- Footer
+    ============================================= -->
+    <footer id="footer" class="dark">
+
+      <!-- Copyrights
+      ============================================= -->
+      <div id="copyrights">
+
+        <div class="container clearfix">
+
+          <div class="col_half">
+            Copyrights &copy; 2017 All Rights Reserved by Udemy.<br>
+            <div class="copyright-links">
+              <a href="#">Privacy Policy</a>
+            </div>
+          </div>
+
+          <div class="col_half col_last tright">
+            <div class="fright clearfix">
+              <a href="#" class="social-icon si-small si-borderless si-facebook">
+                <i class="icon-facebook"></i>
+                <i class="icon-facebook"></i>
+              </a>
+
+              <a href="#" class="social-icon si-small si-borderless si-twitter">
+                <i class="icon-twitter"></i>
+                <i class="icon-twitter"></i>
+              </a>
+
+            </div>
+
+            <div class="clear"></div>
+
+            <i class="icon-envelope2"></i> info@email.com <span class="middot">&middot;</span>
+            <i class="icon-headphones"></i> +55-5-5555-5555
+          </div>
+
+        </div>
+
+      </div><!-- #copyrights end -->
+
+    </footer><!-- #footer end -->
+  </div><!-- #wrapper end -->
+
+  <!-- Go To Top
+  ============================================= -->
+  <div id="gotoTop" class="icon-angle-up"></div>
+
+  <!-- External JavaScripts
+  ============================================= -->
+<?php wp_footer() ?>
+</body>
+
+</html>
