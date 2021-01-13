67121a71ed24c2d7e74ce38e3d549ecf29e5a2b6
deyems <adeyemisola11@gmail.com>
2021-01-13 22:43:28 +0100

    call the custom walker class

--- themes/udemy/functions.php
+++ themes/udemy/functions.php
@@ -6,6 +6,7 @@ define('JU_DEV_MODE', true);
 //Includes
 include (get_theme_file_path('/includes/front/enqueue.php'));
 include (get_theme_file_path('/includes/setup.php'));
+include (get_theme_file_path('includes/custom-nav-walker.php'));
 
 //Hooks
 add_action('wp_enqueue_scripts', 'ju_enqueue');
