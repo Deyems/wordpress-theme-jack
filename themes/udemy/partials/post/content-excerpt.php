bcf115bdf95fc1cca76703b642e6723b7e103a8a
deyems <adeyemisola11@gmail.com>
2021-01-14 14:00:51 +0100

    extracted part of post template

---
+++ themes/udemy/partials/post/content-excerpt.php
@@ -0,0 +1,39 @@
+<div class="entry clearfix">
+    <div class="entry-image">
+        <a href="#">
+            <img class="image_fade" src="images/blog/standard/17.jpg">
+        </a>
+    </div>
+    <div class="entry-title">
+        <h2>
+            <a href="single.html">
+            This is a Standard post with a Preview Image
+            </a>
+        </h2>
+    </div>
+    <ul class="entry-meta clearfix">
+        <li><i class="icon-calendar3"></i> 10th February 2014</li>
+        <li>
+            <a href="#">
+            <i class="icon-user"></i>
+            admin
+            </a>
+        </li>
+        <li>
+            <i class="icon-folder-open"></i>
+            <a href="#">General</a>, <a href="#">Media</a>
+        </li>
+        <li>
+            <a href="#">
+            <i class="icon-comments"></i>
+            13 Comments
+            </a>
+        </li>
+    </ul>
+    <div class="entry-content">
+        <p>
+            <?php the_content() ?>
+        </p>
+        <a href="#" class="more-link">Read More</a>
+    </div>
+</div>
