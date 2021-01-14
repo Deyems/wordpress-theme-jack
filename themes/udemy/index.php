d53fbad813079b76bf03a756a5235cdbda47b797
deyems <adeyemisola11@gmail.com>
2021-01-14 14:00:23 +0100

    removed repeated parts to allow for wordpress looping

--- themes/udemy/index.php
+++ themes/udemy/index.php
@@ -37,134 +37,54 @@
             <!-- Posts
             ============================================= -->
             <div id="posts">
-
-              <div class="entry clearfix">
-                <div class="entry-image">
-                  <a href="#">
-                    <img class="image_fade" src="images/blog/standard/17.jpg">
-                  </a>
-                </div>
-                <div class="entry-title">
-                  <h2>
-                    <a href="single.html">
-                      This is a Standard post with a Preview Image
-                    </a>
-                  </h2>
-                </div>
-                <ul class="entry-meta clearfix">
-                  <li><i class="icon-calendar3"></i> 10th February 2014</li>
-                  <li>
-                    <a href="#">
-                      <i class="icon-user"></i>
-                      admin
-                    </a>
-                  </li>
-                  <li>
-                    <i class="icon-folder-open"></i>
-                    <a href="#">General</a>, <a href="#">Media</a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="icon-comments"></i>
-                      13 Comments
-                    </a>
-                  </li>
-                </ul>
-                <div class="entry-content">
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate,
-                    asperiores quod est tenetur in. Eligendi, deserunt, blanditiis est
-                    quisquam doloribus voluptate id aperiam ea ipsum magni aut perspiciatis
-                    rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat
-                    atque vitae voluptatem libero at eveniet veritatis ab facere.
-                  </p>
-                  <a href="#" class="more-link">Read More</a>
-                </div>
-              </div>
-
-              <div class="entry clearfix">
-                <div class="entry-image">
-                  <iframe src="http://player.vimeo.com/video/87701971" width="500" height="281" frameborder="0"
-                    webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                </div>
-                <div class="entry-title">
-                  <h2><a href="blog-single-full.html">This is a Standard post with an Embedded Video</a></h2>
-                </div>
-                <ul class="entry-meta clearfix">
-                  <li><i class="icon-calendar3"></i> 16th February 2014</li>
-                  <li><a href="#"><i class="icon-user"></i> admin</a></li>
-                  <li><i class="icon-folder-open"></i> <a href="#">Videos</a>, <a href="#">News</a></li>
-                  <li><a href="blog-single-full.html#comments"><i class="icon-comments"></i> 19 Comments</a></li>
-                </ul>
-                <div class="entry-content">
-                  <p>Asperiores, tenetur, blanditiis, quaerat odit ex exercitationem pariatur quibusdam veritatis
-                    quisquam laboriosam esse beatae hic perferendis velit deserunt soluta iste repellendus officia in
-                    neque veniam debitis placeat quo unde reprehenderit eum facilis vitae. Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit. Nihil, reprehenderit!</p>
-                  <a href="blog-single-full.html" class="more-link">Read More <i class="icon-caret-right"></i></a>
-                </div>
-              </div>
-
-              <div class="entry clearfix">
-                <div class="entry-image">
-                  <div class="fslider" data-arrows="false" data-lightbox="gallery">
-                    <div class="flexslider">
-                      <div class="slider-wrap">
-                        <div class="slide"><a href="images/blog/full/10.jpg" data-lightbox="gallery-item"><img
-                              class="image_fade" src="images/blog/standard/10.jpg" alt="Standard Post with Gallery"></a>
-                        </div>
-                        <div class="slide"><a href="images/blog/full/20.jpg" data-lightbox="gallery-item"><img
-                              class="image_fade" src="images/blog/standard/20.jpg" alt="Standard Post with Gallery"></a>
-                        </div>
-                        <div class="slide"><a href="images/blog/full/21.jpg" data-lightbox="gallery-item"><img
-                              class="image_fade" src="images/blog/standard/21.jpg" alt="Standard Post with Gallery"></a>
-                        </div>
+                <?php
+                    if(have_posts()){
+                        while(have_posts()){
+                            the_post();
+                ?>
+                    <div class="entry clearfix">
+                      <div class="entry-image">
+                        <a href="#">
+                          <img class="image_fade" src="images/blog/standard/17.jpg">
+                        </a>
+                      </div>
+                      <div class="entry-title">
+                        <h2>
+                          <a href="single.html">
+                            This is a Standard post with a Preview Image
+                          </a>
+                        </h2>
+                      </div>
+                      <ul class="entry-meta clearfix">
+                        <li><i class="icon-calendar3"></i> 10th February 2014</li>
+                        <li>
+                          <a href="#">
+                            <i class="icon-user"></i>
+                            admin
+                          </a>
+                        </li>
+                        <li>
+                          <i class="icon-folder-open"></i>
+                          <a href="#">General</a>, <a href="#">Media</a>
+                        </li>
+                        <li>
+                          <a href="#">
+                            <i class="icon-comments"></i>
+                            13 Comments
+                          </a>
+                        </li>
+                      </ul>
+                      <div class="entry-content">
+                        <p>
+                          <?php the_content() ?>
+                        </p>
+                        <a href="#" class="more-link">Read More</a>
                       </div>
                     </div>
-                  </div>
-                </div>
-                <div class="entry-title">
-                  <h2><a href="blog-single-small.html">This is a Standard post with a Slider Gallery</a></h2>
-                </div>
-                <ul class="entry-meta clearfix">
-                  <li><i class="icon-calendar3"></i> 24th February 2014</li>
-                  <li><a href="#"><i class="icon-user"></i> admin</a></li>
-                  <li><i class="icon-folder-open"></i> <a href="#">Gallery</a>, <a href="#">Media</a></li>
-                  <li><a href="blog-single-small.html#comments"><i class="icon-comments"></i> 21 Comments</a></li>
-                </ul>
-                <div class="entry-content">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, voluptatem, dolorem animi nisi
-                    autem blanditiis enim culpa reiciendis et explicabo tenetur voluptate rerum molestiae eaque possimus
-                    exercitationem eligendi fuga. Maiores, sunt eveniet doloremque porro hic exercitationem distinctio
-                    sequi adipisci. Nulla, fuga perferendis voluptatum beatae voluptate architecto laboriosam provident
-                    deserunt. Saepe!</p>
-                  <a href="blog-single-small.html" class="more-link">Read More</a>
-                </div>
-              </div>
-
-              <div class="entry clearfix">
-                <div class="entry-image clearfix">
-                  <iframe width="100%" scrolling="no" frameborder="no"
-                    src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/115823769&amp;auto_play=false&amp;hide_related=true&amp;visual=true"></iframe>
-                </div>
-                <div class="entry-title">
-                  <h2><a href="single.html">This is an Embedded Audio Post</a></h2>
-                </div>
-                <ul class="entry-meta clearfix">
-                  <li><i class="icon-calendar3"></i> 28th April 2014</li>
-                  <li><a href="#"><i class="icon-user"></i> admin</a></li>
-                  <li><i class="icon-folder-open"></i> <a href="#">Audio</a>, <a href="#">General</a></li>
-                  <li><a href="single.html#comments"><i class="icon-comments"></i> 16 Comments</a></li>
-                </ul>
-                <div class="entry-content">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione, voluptatem, dolorem animi nisi
-                    autem blanditiis enim culpa reiciendis et explicabo tenetur voluptate rerum molestiae eaque possimus
-                    exercitationem eligendi fuga. Maiores, sunt eveniet doloremque porro hic exercitationem distinctio
-                    sequi adipisci. Nulla, fuga perferendis voluptatum beatae voluptate architecto laboriosam provident
-                    deserunt. Saepe!</p>
-                  <a href="single.html" class="more-link">Read More</a>
-                </div>
-              </div>
+                <?php
+                        }
+                    }
+                ?>
 
             </div><!-- #posts end -->
 
