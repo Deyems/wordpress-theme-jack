cfc6980dd0fab6cee50ea748f2e4e6359f5f1b27
deyems <adeyemisola11@gmail.com>
2021-01-14 20:31:44 +0100

    adding pagination functionality

--- themes/udemy/index.php
+++ themes/udemy/index.php
@@ -56,14 +56,14 @@
             <!-- Pagination
             ============================================= -->
             <div class="row mb-3">
-              <div class="col-12">
-                <a href="#" class="btn btn-outline-secondary float-left">
-                  &larr; Older
-                </a>
-                <a href="#" class="btn btn-outline-dark float-right">
-                  Newer &rarr;
-                </a>
-              </div>
+                <div class="col-12">
+                    <?php next_posts_link( '&larr; Older' ); ?>
+                    <!-- <a href="#" class="btn btn-outline-secondary float-left">
+                    </a>
+                    <a href="#" class="btn btn-outline-dark float-right">
+                    </a> -->
+                    <?php previous_posts_link( 'Newer &rarr;' ); ?>
+                </div>
             </div>
             <!-- .pager end -->
 
