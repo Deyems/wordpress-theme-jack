f23822738f7b1850aade7b2493f7e04d2f748e8f
deyems <adeyemisola11@gmail.com>
2021-01-14 18:16:27 +0100

    separating concerns on index page

--- themes/udemy/index.php
+++ themes/udemy/index.php
@@ -41,46 +41,11 @@
                     if(have_posts()){
                         while(have_posts()){
                             the_post();
+                            get_template_part('partials/post/content', 'excerpt');
                 ?>
-                    <div class="entry clearfix">
-                      <div class="entry-image">
-                        <a href="#">
-                          <img class="image_fade" src="images/blog/standard/17.jpg">
-                        </a>
-                      </div>
-                      <div class="entry-title">
-                        <h2>
-                          <a href="single.html">
-                            This is a Standard post with a Preview Image
-                          </a>
-                        </h2>
-                      </div>
-                      <ul class="entry-meta clearfix">
-                        <li><i class="icon-calendar3"></i> 10th February 2014</li>
-                        <li>
-                          <a href="#">
-                            <i class="icon-user"></i>
-                            admin
-                          </a>
-                        </li>
-                        <li>
-                          <i class="icon-folder-open"></i>
-                          <a href="#">General</a>, <a href="#">Media</a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i class="icon-comments"></i>
-                            13 Comments
-                          </a>
-                        </li>
-                      </ul>
-                      <div class="entry-content">
-                        <p>
-                          <?php the_content() ?>
-                        </p>
-                        <a href="#" class="more-link">Read More</a>
-                      </div>
-                    </div>
+                    <!-- <div class="entry clearfix">
+                      <?php the_content() ?>
+                    -->
                 <?php
                         }
                     }
