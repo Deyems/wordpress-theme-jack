b149ddeaca6143806b44bfed7f468ee2a955dd34
deyems <adeyemisola11@gmail.com>
2021-01-14 01:51:09 +0100

    moving sidebar logic to its own file

--- themes/udemy/index.php
+++ themes/udemy/index.php
@@ -184,44 +184,7 @@
 
           </div><!-- .postcontent end -->
 
-          <!-- Sidebar
-          ============================================= -->
-          <div class="sidebar nobottommargin col_last">
-            <div class="sidebar-widgets-wrap">
-
-              <div class="widget clearfix">
-                <h4>Recipes of the Day</h4>
-
-                <div class="portfolio-image">
-                  <a href="#">
-                    <img src="images/portfolio/4/3.jpg" alt="Mac Sunglasses">
-                  </a>
-                </div>
-                <div class="portfolio-desc center nobottompadding">
-                  <h3><a href="portfolio-single-video.html">Mac Sunglasses</a></h3>
-                </div>
-              </div>
-
-              <div class="widget clearfix">
-
-                <h4>Tag Cloud</h4>
-                <div class="tagcloud">
-                  <a href="#">general</a>
-                  <a href="#">videos</a>
-                  <a href="#">music</a>
-                  <a href="#">media</a>
-                  <a href="#">photography</a>
-                  <a href="#">parallax</a>
-                  <a href="#">ecommerce</a>
-                  <a href="#">terms</a>
-                  <a href="#">coupons</a>
-                  <a href="#">modern</a>
-                </div>
-
-              </div>
-            </div>
-
-          </div><!-- .sidebar end -->
+          <?php get_sidebar(); ?>
 
         </div>
 
