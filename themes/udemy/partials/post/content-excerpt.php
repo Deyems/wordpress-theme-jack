3f58aa476144b8e8932e0610113e4734476ac8bf
deyems <adeyemisola11@gmail.com>
2021-01-14 18:13:23 +0100

    converting static assets to Template tags

--- themes/udemy/partials/post/content-excerpt.php
+++ themes/udemy/partials/post/content-excerpt.php
@@ -1,39 +1,45 @@
 <div class="entry clearfix">
-    <div class="entry-image">
-        <a href="#">
-            <img class="image_fade" src="images/blog/standard/17.jpg">
-        </a>
-    </div>
+    <?php if(has_post_thumbnail()){ ?>
+        <div class="entry-image">
+            <a href="<?php the_permalink(); ?>">
+                <?php 
+                    the_post_thumbnail( 'full', [
+                        'class' => 'image_fade'
+                    ] ); 
+                ?>
+            </a>
+        </div>
+    <?php } ?>
     <div class="entry-title">
         <h2>
-            <a href="single.html">
-            This is a Standard post with a Preview Image
+            <a href="<?php the_permalink(); ?>">
+            <?php the_title(); ?>
             </a>
         </h2>
     </div>
     <ul class="entry-meta clearfix">
-        <li><i class="icon-calendar3"></i> 10th February 2014</li>
+        <li><i class="icon-calendar3"></i> <?php echo get_the_date(); ?></li>
         <li>
-            <a href="#">
+            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
             <i class="icon-user"></i>
-            admin
+            <?php the_author(); ?>
             </a>
         </li>
         <li>
             <i class="icon-folder-open"></i>
-            <a href="#">General</a>, <a href="#">Media</a>
+            <?php the_category( ',' ); ?>
         </li>
         <li>
             <a href="#">
             <i class="icon-comments"></i>
-            13 Comments
+            <?php comments_number( '0' ); ?>
             </a>
         </li>
     </ul>
     <div class="entry-content">
         <p>
-            <?php the_content() ?>
+            <?php the_excerpt(); ?>
         </p>
-        <a href="#" class="more-link">Read More</a>
+        <a href="<?php the_permalink(); ?>" class="more-link">Read More</a>
     </div>
 </div>
