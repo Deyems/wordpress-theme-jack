6f2125a8b656b1201323fe8bdb9af05f3981ced2
deyems <adeyemisola11@gmail.com>
2021-01-14 01:50:30 +0100

    include widgets -> sidebar stuff

--- themes/udemy/functions.php
+++ themes/udemy/functions.php
@@ -7,9 +7,11 @@ define('JU_DEV_MODE', true);
 include (get_theme_file_path('/includes/front/enqueue.php'));
 include (get_theme_file_path('/includes/setup.php'));
 include (get_theme_file_path('includes/custom-nav-walker.php'));
+include (get_theme_file_path('includes/widgets.php'));
 
 //Hooks
 add_action('wp_enqueue_scripts', 'ju_enqueue');
 add_action('after_setup_theme', 'ju_setup_theme');
+add_action('widgets_init', 'ju_widgets');
 
 //Shortcodes
