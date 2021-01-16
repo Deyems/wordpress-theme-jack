4dd7974070758e67a2314a9d97b75d24e13054d3
deyems <adeyemisola11@gmail.com>
2021-01-16 19:07:37 +0100

    including path to misc customizer

--- themes/udemy/functions.php
+++ themes/udemy/functions.php
@@ -10,6 +10,7 @@ include (get_theme_file_path('includes/custom-nav-walker.php'));
 include (get_theme_file_path('includes/widgets.php'));
 include (get_theme_file_path('includes/theme-customizer.php'));
 include (get_theme_file_path('includes/customizer/social.php'));
+include (get_theme_file_path('includes/customizer/misc.php'));
 
 //Hooks
 add_action('wp_enqueue_scripts', 'ju_enqueue');
