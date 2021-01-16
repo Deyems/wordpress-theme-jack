4ece9aec5523be2205528a92552ecefa3882d5e8
deyems <adeyemisola11@gmail.com>
2021-01-16 12:54:09 +0100

    adding a call to quads_register_ad

--- themes/udemy/includes/setup.php
+++ themes/udemy/includes/setup.php
@@ -6,4 +6,12 @@ function ju_setup_theme(){
     add_theme_support( 'post-thumbnails' );
     register_nav_menu( 'primary', __( 'Primary Menu', 'udemy' ) );
     register_nav_menu( 'secondary', __( 'Secondary Menu', 'udemy' ) );
+
+    if (function_exists('quads_register_ad')){
+        quads_register_ad([
+            'location' => 'udemy_header',
+            'description' => 'Udemy Header position'
+        ]);
+    }
+
 }
