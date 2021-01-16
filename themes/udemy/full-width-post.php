1bafe907e25e876d040b9560a41bc42a878b2a9f
deyems <adeyemisola11@gmail.com>
2021-01-16 01:28:52 +0100

    full-width-design-for-post-types

---
+++ themes/udemy/full-width-post.php
@@ -0,0 +1,86 @@
+<?php 
+/* 
+ * Template Name: Full Width Post
+ * Template Post Type: Post
+*/
+
+get_header(); ?>
+    <!-- Page Title ============================================= -->
+    <section id="page-title">
+        <div class="container clearfix">
+            <h1><?php single_post_title(); ?></h1>
+            <span>
+                <?php 
+                    if(function_exists('the_subtitle')){
+                        the_subtitle();
+                    }
+                ?>
+            </span>
+        </div>
+    </section><!-- #page-title end -->
+    
+    <!-- Content ============================================= -->
+    <section id="content">
+
+        <div class="content-wrap">
+
+            <div class="container clearfix">
+
+                <!-- Post Content ============================================= -->
+                <?php 
+                    while(have_posts()){
+                        the_post();
+                        global $post;
+                        $author_ID = $post->post_author;
+                        $author_URL = get_author_posts_url($author_ID);
+                        ?>
+                    
+                        <!-- Single Post ============================================= -->
+                        <div class="entry clearfix">
+
+                            <!-- Entry Image ============================================= -->
+                            <div class="entry-image">
+                                <?php if(has_post_thumbnail()){ ?>
+                                        <a href="<?php the_permalink(); ?>">
+                                            <?php 
+                                                the_post_thumbnail( 'full' );
+                                            ?>
+                                        </a>
+                                <?php } ?>
+                            </div><!-- .entry-image end -->
+
+                            <!-- Entry Content ============================================= -->
+                            <div class="entry-content notopmargin">
+
+                                <!-- Post Single - Content End -->
+                                <?php the_excerpt();
+
+                                    $defaults = array(
+                                        'before' => '<p class="text-center">' . __( 'Pages:', 'udemy' ),
+                                        'after' => '</p>',
+                                    );
+
+                                    wp_link_pages( $defaults );
+                                ?>
+
+                                <div class="clear"></div>
+
+                            </div>
+                        </div><!-- .entry end -->
+
+                        <?php
+                            if(comments_open() || get_comments_number()) {
+                                comments_template();
+                            }
+                        ?>
+                    <?php
+                        }
+                    ?>
+
+            </div>
+
+        </div>
+
+    </section><!-- #content end -->
+
+<?php get_footer(); ?>
