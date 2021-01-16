be9b5194bbfe0542c8b6e2cee67effe7cb6c1900
deyems <adeyemisola11@gmail.com>
2021-01-16 16:57:51 +0100

    add the path to call the functions

--- themes/udemy/functions.php
+++ themes/udemy/functions.php
@@ -8,10 +8,13 @@ include (get_theme_file_path('/includes/front/enqueue.php'));
 include (get_theme_file_path('/includes/setup.php'));
 include (get_theme_file_path('includes/custom-nav-walker.php'));
 include (get_theme_file_path('includes/widgets.php'));
+include (get_theme_file_path('includes/theme-customizer.php'));
+include (get_theme_file_path('includes/customizer/social.php'));
 
 //Hooks
 add_action('wp_enqueue_scripts', 'ju_enqueue');
 add_action('after_setup_theme', 'ju_setup_theme');
 add_action('widgets_init', 'ju_widgets');
+add_action('customize_register', 'ju_customize_register');
 
 //Shortcodes
