6dedf584cf55ff52e95cc82224fa46be273ab2c4
deyems <adeyemisola11@gmail.com>
2021-01-17 16:29:32 +0100

    including panel key to section

--- themes/udemy/includes/customizer/social.php
+++ themes/udemy/includes/customizer/social.php
@@ -21,7 +21,8 @@ function ju_social_customizer_section($wp_customize){
     //Sections
     $wp_customize->add_section('ju_social_section', [
         'title' => __('Deyems Social Settings', 'udemy'),
-        'priority' => 30
+        'priority' => 30,
+        'panel' => 'deyems'
     ]);
     
     //Controls
