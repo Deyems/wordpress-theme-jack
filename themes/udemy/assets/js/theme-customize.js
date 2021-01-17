18e3e4644b9d82fc190579640c75d939d5f8bcb1
deyems <adeyemisola11@gmail.com>
2021-01-17 16:30:05 +0100

    script to stop page-reload while customizing

--- themes/udemy/assets/js/theme-customize.js
+++ themes/udemy/assets/js/theme-customize.js
@@ -1,6 +1,25 @@
 /**
  * Created by test on 3/27/2017.
+ * Modified by Sola on 01/17/2021
  */
 (function($){
-
+    wp.customize('ju_header_show_search', function( value ) {
+        value.bind(function( new_val ) {
+            if(new_val){
+                $("#top-search").show();
+            }else{
+                $("#top-search").hide();
+            }
+        });
+    });
+    
+    wp.customize('ju_header_show_cart', function( value ) {
+        value.bind(function( new_val ) {
+            if(new_val){
+                $("#top-cart").show();
+            }else{
+                $("#top-cart").hide();
+            }
+        });
+    });
 })(jQuery);
