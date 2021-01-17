05ecdee39be499cf547c3d28db66ac07e21759c8
deyems <adeyemisola11@gmail.com>
2021-01-17 16:29:11 +0100

    adding panel to customizer and changing existing section on customizer

--- themes/udemy/includes/theme-customizer.php
+++ themes/udemy/includes/theme-customizer.php
@@ -1,6 +1,18 @@
 <?php 
 
 function ju_customize_register($wp_customize){
+    // echo '<pre>';
+    // var_dump($wp_customize);
+    // echo '</pre>';
+    
+    $wp_customize->get_section('title_tagline')->title = 'General';
+    
+    $wp_customize->add_panel('deyems', [
+        'title' => __('Deyems', 'udemy'),
+        'description' => '<p>Deyems Theme Settings</p>',
+        'priority' => 160
+    ]);
+
     ju_social_customizer_section($wp_customize);
     ju_misc_customizer_section($wp_customize);
 }
