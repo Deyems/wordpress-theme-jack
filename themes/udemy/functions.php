d1ef4b1275faff4e3106ae01ccfebae92e97ea72
deyems <adeyemisola11@gmail.com>
2021-01-17 16:31:05 +0100

    adjusting customizer to handle page load - hooking to customize_preview_init

--- themes/udemy/functions.php
+++ themes/udemy/functions.php
@@ -11,11 +11,13 @@ include (get_theme_file_path('includes/widgets.php'));
 include (get_theme_file_path('includes/theme-customizer.php'));
 include (get_theme_file_path('includes/customizer/social.php'));
 include (get_theme_file_path('includes/customizer/misc.php'));
+include (get_theme_file_path('includes/customizer/enqueue.php'));
 
 //Hooks
 add_action('wp_enqueue_scripts', 'ju_enqueue');
 add_action('after_setup_theme', 'ju_setup_theme');
 add_action('widgets_init', 'ju_widgets');
 add_action('customize_register', 'ju_customize_register');
+add_action( 'customize_preview_init' , 'ju_customize_preview_init' );
 
 //Shortcodes
