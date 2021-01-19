024cdb90e22836bd14ea7422d517128b92633b70
deyems <adeyemisola11@gmail.com>
2021-01-19 16:48:45 +0100

    proper id naming

--- plugins/recipe/process/recipe-template.php
+++ plugins/recipe/process/recipe-template.php
@@ -1,6 +1,6 @@
 <ul class="list-unstyled">
     <li>
         <strong>RATE_I18N: </strong>
-        <div id="recipe-rating" class="rateit" data-rateit-resetable="false" data-rid="RECIPE_ID"></div>
+        <div id="recipe_rating" class="rateit" data-rateit-resetable="false" data-rid="RECIPE_ID"></div>
     </li>
 </ul>
