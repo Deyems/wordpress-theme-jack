4eed96ef380bcb8690621e6b64ca3bfe6b33b7be
deyems <adeyemisola11@gmail.com>
2021-01-19 14:19:36 +0100

    filter hook function to replace some content in template to inject

--- plugins/recipe/process/filter-content.php
+++ plugins/recipe/process/filter-content.php
@@ -7,6 +7,7 @@ function r_filter_recipe_content($content){
     global $post;
     $recipe_data = get_post_meta($post->ID, 'recipe_data', true);
     $recipe_html = file_get_contents('recipe-template.php', true);
-    $recipe_html = str_replace('RATE_I18N', __('Rating', 'udemy'), $recipe_html );
-    return $recipe_html . $content;
+    $recipe_html = str_replace('RATE_I18N:', __("Rating", 'udemy'), $recipe_html );
+    $recipe_html = str_replace('RECIPE_ID', $post->ID, $recipe_html );
+    return "<div>$recipe_html</div>" . $content;
 }
