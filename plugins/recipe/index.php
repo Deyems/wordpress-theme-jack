e789ec5fe56ba93e42bf3a96077f70736422fd7e
deyems <adeyemisola11@gmail.com>
2021-01-19 14:17:00 +0100

    enqueueing script to plugin

--- plugins/recipe/index.php
+++ plugins/recipe/index.php
@@ -14,13 +14,14 @@ if(!function_exists('add_action')) {
 }
 
 //Set ups
-
+define('RECIPE_PLUGIN_URL', __FILE__);
 
 //Includes
 require_once __DIR__ . "/includes/activate.php";
 require_once __DIR__ . "/includes/init.php";
 require_once __DIR__ . '/process/save-post.php';
 require_once __DIR__ . '/process/filter-content.php';
+require_once __DIR__ . "/includes/front/enqueue-script.php";
 
 //Hooks
 register_activation_hook( __FILE__, 'r_activate_plugin' );
@@ -29,6 +30,7 @@ add_action( 'init', 'recipe_init');
 //Flexible Save Post {Post_type} Hook.
 add_action( 'save_post_recipes', 'r_save_post_admin', 10, 3 );
 add_filter( 'the_content', 'r_filter_recipe_content' );
+add_action( 'wp_enqueue_scripts', 'r_enqueue_scripts', 100 );
 
 //Shortcodes
 
