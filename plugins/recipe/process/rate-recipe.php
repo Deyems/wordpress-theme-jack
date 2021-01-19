c7dd4ba0362b3ca0dc5625bb2fda4228d8e4950f
deyems <adeyemisola11@gmail.com>
2021-01-19 22:15:38 +0100

    performing inserting to db operation

--- plugins/recipe/process/rate-recipe.php
+++ plugins/recipe/process/rate-recipe.php
@@ -1,6 +1,30 @@
 <?php 
-
 function r_rate_recipe(){
-    print_r($_POST);
-    // die();
+   global $wpdb;
+   $table_name = $wpdb->prefix . "recipe_ratings";
+   $output = ['status' => 1];
+   $postID = absint($_POST['rid']);
+   $rating = round($_POST['rating'], 1);
+   $user_IP = $_SERVER['REMOTE_ADDR'];
+
+   //Perform Check to stop multiple submissions to Db
+   $rating_count = $wpdb->get_var(
+    "SELECT COUNT(*) FROM `". $table_name. "` 
+    WHERE recipe_id='". $postID ."' AND user_ip= '". $user_IP. "'"
+   );
+
+   if($rating_count>0){
+    wp_send_json($output);
+   }
+   
+   $wpdb->insert( $table_name,
+    [
+        'recipe_id' => $postID,
+        'rating' => $rating,
+        'user_ip' => $user_IP
+    ],
+    [ '%d', '%f', '%s' ]
+   );
+   $output['status'] = 2;
+   wp_send_json($output);
 }
