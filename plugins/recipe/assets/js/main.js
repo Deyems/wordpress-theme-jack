d6f28772af846e8666fff26d55e2bad0c998da1d
deyems <adeyemisola11@gmail.com>
2021-01-19 22:16:32 +0100

    removed old code and some unused lines

--- plugins/recipe/assets/js/main.js
+++ plugins/recipe/assets/js/main.js
@@ -1,31 +1,13 @@
-// (function($){
-//     $("#recipe_rating").bind( 'rated', function(){
-//         $(this).rateit( 'readonly', true );
-
-//         var form        =   {
-//             action:         'r_rate_recipe',
-//             rid:            $(this).data( 'rid' ),
-//             rating:         $(this).rateit( 'value' )
-//         };
-
-//         $.post( recipe_obj.ajax_url, form, function(data){
-            
-//         });
-//     });
-// })(jQuery);
-
 (function($){
     $("#recipe_rating").bind('rated', function(){
-        // alert('Touched something here');
         $(this).rateit( 'readonly', true );
         var form = {
             action: 'r_rate_recipe',
             rid: $(this).data( 'rid' ),
             rating: $(this).rateit( 'value' )
         }
-        //  console.log(recipe_obj.ajax_url);
         $.post(recipe_obj.ajax_url, form, function (data){
-            console.log(data);
+            
         });
         
     });
