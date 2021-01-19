ce9f3b5ca68147de5db9f1c0c37abab1543feeb9
deyems <adeyemisola11@gmail.com>
2021-01-19 16:50:33 +0100

    resolving plugins_url issue and localize script

--- plugins/recipe/includes/front/enqueue-script.php
+++ plugins/recipe/includes/front/enqueue-script.php
@@ -1,20 +1,26 @@
 <?php
 
 function r_enqueue_scripts(){
-    wp_register_style('r_rateit', plugins_url('/assets/rateit/rateit.css'), );
+    wp_register_style('r_rateit', 
+    plugins_url('/assets/rateit/rateit.css', RECIPE_PLUGIN_URL));
     wp_enqueue_style('r_rateit');
     
     wp_register_script('r_rate_it', 
-    plugins_url('/assets/rateit/jquery.rateit.min.js'),
+    plugins_url('/assets/rateit/jquery.rateit.min.js', RECIPE_PLUGIN_URL),
     ['jquery'],
     '1.0.0',
     true);
     
     wp_register_script('r_main', 
-    plugins_url('/assets/js/main.js'),
+    plugins_url('/assets/js/main.js', RECIPE_PLUGIN_URL),
     ['jquery'],
     '1.0.0',
     true);
+
+    wp_localize_script('r_main', 'recipe_obj', [
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'nonce' => wp_create_nonce('wp_rest'),
+    ]);
 
     wp_enqueue_script('r_rate_it');
     wp_enqueue_script('r_main');
