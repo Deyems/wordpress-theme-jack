fb801a05e1826dcadbd1b782491906c27fad338d
deyems <adeyemisola11@gmail.com>
2021-01-20 00:58:32 +0100

    adding attributes to control readonly after user rating

--- plugins/recipe/process/recipe-template.php
+++ plugins/recipe/process/recipe-template.php
@@ -1,7 +1,9 @@
 <ul class="list-unstyled">
     <li>
         <strong>RATE_I18N: </strong>
-        <div id="recipe_rating" class="rateit" 
-        data-rateit-resetable="false" data-rid="RECIPE_ID"></div>
+            <div id="recipe_rating" class="rateit" 
+                READONLY_PLACEHOLDER data-rateit-value="RECIPE_RATING"
+                data-rateit-resetable="false" data-rid="RECIPE_ID">
+            </div>
     </li>
 </ul>
