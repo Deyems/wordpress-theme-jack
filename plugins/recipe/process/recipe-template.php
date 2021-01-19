afc3607fc4b6eef4efe8af91a59cd47f5c53640e
deyems <adeyemisola11@gmail.com>
2021-01-19 22:16:00 +0100

    adjusting line

--- plugins/recipe/process/recipe-template.php
+++ plugins/recipe/process/recipe-template.php
@@ -1,6 +1,7 @@
 <ul class="list-unstyled">
     <li>
         <strong>RATE_I18N: </strong>
-        <div id="recipe_rating" class="rateit" data-rateit-resetable="false" data-rid="RECIPE_ID"></div>
+        <div id="recipe_rating" class="rateit" 
+        data-rateit-resetable="false" data-rid="RECIPE_ID"></div>
     </li>
 </ul>
