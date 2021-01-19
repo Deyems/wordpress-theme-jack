c0d97168bf0a48a5c41923d2073758233c2314e9
deyems <adeyemisola11@gmail.com>
2021-01-19 16:47:33 +0100

    moved

--- plugins/assets/js/admin-global.js
+++
@@ -1,9 +0,0 @@
-(function($){
-    $(document).on( "click", "#r-recipe-pending-notice .notice-dismiss",  function(e){
-        e.preventDefault();
-
-        $.post( ajaxurl, {
-            action:         'r_dismiss_pending_recipe_notice'
-        });
-    });
-})(jQuery);
