f0f36cdf7c45d41909fca731734edf047749d879
deyems <adeyemisola11@gmail.com>
2021-01-20 00:59:29 +0100

    adding a way to get and update meta data

--- plugins/recipe/process/rate-recipe.php
+++ plugins/recipe/process/rate-recipe.php
@@ -2,6 +2,7 @@
 function r_rate_recipe(){
    global $wpdb;
    $table_name = $wpdb->prefix . "recipe_ratings";
+
    $output = ['status' => 1];
    $postID = absint($_POST['rid']);
    $rating = round($_POST['rating'], 1);
@@ -10,13 +11,14 @@ function r_rate_recipe(){
    //Perform Check to stop multiple submissions to Db
    $rating_count = $wpdb->get_var(
     "SELECT COUNT(*) FROM `". $table_name. "` 
-    WHERE recipe_id='". $postID ."' AND user_ip= '". $user_IP. "'"
+    WHERE recipe_id='". $postID ."' AND user_ip= '". $user_IP . "'"
    );
 
    if($rating_count>0){
     wp_send_json($output);
    }
    
+   //Insert Rating into DB
    $wpdb->insert( $table_name,
     [
         'recipe_id' => $postID,
@@ -25,6 +27,21 @@ function r_rate_recipe(){
     ],
     [ '%d', '%f', '%s' ]
    );
+
+   //Update Rating META VALUES
+   $recipe_data = get_post_meta($postID,'recipe_data', true);
+
+   $recipe_data['rating_count']++;
+   $recipe_data['rating'] = round($wpdb->get_var(
+    "SELECT AVG(`rating`) FROM `". $table_name. "` 
+    WHERE recipe_id='". $postID ."'"
+   ), 1);
+    echo "<pre>";
+    print_r($recipe_data);
+    print_r("At Update stage here");
+    echo "</pre>";
+
+   update_post_meta($postID, 'recipe_data', $recipe_data);
    $output['status'] = 2;
    wp_send_json($output);
 }
