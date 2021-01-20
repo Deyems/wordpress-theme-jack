3de97ca8b87655774845cd4510bc73c754c2c4a8
deyems <adeyemisola11@gmail.com>
2021-01-20 17:51:47 +0100

    register block type with various settings

--- plugins/recipe/blocks/app/recipe-block/index.js
+++ plugins/recipe/blocks/app/recipe-block/index.js
@@ -1,5 +1,27 @@
-// console.log(wp);
+const { block_icons } = require("../icons/index");
 
-wp.blocks.registerBlockType('udemy/recipe', {
-    
+// console.log(wp);
+const { registerBlockType } = wp.blocks;
+const { __ } = wp.i18n;
+
+registerBlockType('udemy/recipe', {
+    title: __('Recipe', 'recipe'),
+    description: __('Provides a short summary of a recipe', 'recipe'),
+    //Text, Design, Widget, Embed, Media
+    category: __('common', 'recipe'),
+    icon: block_icons.wapuu,
+    keywords: [
+        __('Food', 'recipe'),
+        __('Ingredients', 'recipe'),
+        __('Meal Type', 'recipe'),
+    ],
+    suppport: {
+        html: false,
+    },
+    edit: () => {
+        return <p> Hello World</p>
+    },
+    save: () => {
+        return <p> Hello World</p>
+    }
 });
