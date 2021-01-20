1b365f11d6ba941467bf49979d6131ca3e58beb4
deyems <adeyemisola11@gmail.com>
2021-01-20 13:33:18 +0100

    starter set up for block building

---
+++ plugins/recipe/blocks/webpack.config.js
@@ -0,0 +1,22 @@
+const path              =   require( 'path' );
+const webpack           =   require( 'webpack' );
+
+module.exports          =   {
+    entry:                  './app/index.js',
+    output: {
+        path:               path.resolve( __dirname, 'dist' ),
+        filename:           'bundle.js',
+    },
+    mode:                   'development',
+    watch:                  true,
+    devtool:                'cheap-eval-source-map',
+    module: {
+        rules: [
+            {
+                test:       /\.js$/,
+                exclude:    /(node_modules)/,
+                use:        'babel-loader',
+            }
+        ]
+    }
+};
