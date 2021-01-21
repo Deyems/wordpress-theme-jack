1ac4583f40ae2ec5eb74b203338d4ebcb7b78c95
deyems <adeyemisola11@gmail.com>
2021-01-21 22:09:34 +0100

    rich-text block script

---
+++ plugins/recipe/blocks/app/richtext-block/index.js
@@ -0,0 +1,46 @@
+import {block_icons} from '../icons/index';
+
+const { registerBlockType } = wp.blocks;
+const { __ } = wp.i18n;
+const { RichText } = wp.editor;
+
+registerBlockType( 'udemy/rich-text', {
+    title:                              __( 'Rich Text Example', 'recipe' ),
+    description:                        __( 'Rich text example', 'recipe' ),
+    tags:                        __( 'Rich text, text editor ', 'recipe' ),
+    category:                           'common',
+    icon:                               block_icons.wapuu,
+    attributes: {
+        message: {
+            type: 'array',
+            source: 'children',
+            selector: '.message-ctr'
+        }
+    },
+    edit: ( props ) => {
+        return (
+            <div className={ props.className }>
+                <h3>Rich Text Example Block</h3>
+                <RichText
+                tagName="div"
+                multiline="p"
+                placeholder={ __('Add your content here', 'recipe') }
+                onChange={(new_val) => {
+                    props.setAttributes({message: new_val})
+                }}
+                value={props.attributes.message}
+                />
+            </div>
+        );
+    },
+    save: ( props ) => {
+        return (
+            <div>
+                <h3>Good job</h3>
+                <div className="message-ctr">
+                    {props.attributes.message}
+                </div>
+            </div>
+        )
+    }
+});
