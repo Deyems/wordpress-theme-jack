df280efb7afef555782c384afd9d990662be67bd
deyems <adeyemisola11@gmail.com>
2021-01-21 10:29:12 +0100

    enqueued google fonts

--- plugins/recipe/blocks/enqueue.php
+++ plugins/recipe/blocks/enqueue.php
@@ -10,3 +10,19 @@ function r_enqueue_block_editor_assets(){
     );
     wp_enqueue_script('r_blocks_bundle');
 }
+
+function r_enqueue_block_assets(){
+    wp_register_style('r_blocks',
+    plugins_url('/blocks/dist/blocks-main.css', RECIPE_PLUGIN_URL),
+    [],
+    filemtime(plugin_dir_path(RECIPE_PLUGIN_URL) . '/blocks/dist/blocks-main.css')
+    );
+    wp_enqueue_style('r_blocks');
+    
+    wp_register_style('r_web_fonts',
+    'https://fonts.googleapis.com/css2?family=Lato&display=swap',
+    ['https://fonts.gstatic.com'],
+    filemtime(plugin_dir_path(RECIPE_PLUGIN_URL) . '/blocks/dist/blocks-main.css')
+    );
+    wp_enqueue_style('r_web_fonts');
+}
