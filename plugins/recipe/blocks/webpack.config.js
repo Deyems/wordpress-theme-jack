5950e3dc5b65dfaf0cc5065bb8b21a39683b49f9
deyems <adeyemisola11@gmail.com>
2021-01-21 01:48:11 +0100

    added raw-loader node-sass sass-loader css-loader mini-css-extract-plugin and made their settings in webpack-config

--- plugins/recipe/blocks/webpack.config.js
+++ plugins/recipe/blocks/webpack.config.js
@@ -1,5 +1,11 @@
 const path              =   require( 'path' );
 const webpack           =   require( 'webpack' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+
+//Extract CSS for Gutenberg Block
+const editor_css_plugin = new MiniCssExtractPlugin({
+    filename: 'blocks-[name].css'
+});
 
 module.exports          =   {
     entry:                  './app/index.js',
@@ -16,7 +22,18 @@ module.exports          =   {
                 test:       /\.js$/,
                 exclude:    /(node_modules)/,
                 use:        'babel-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        editor_css_plugin
+    ]
 };
