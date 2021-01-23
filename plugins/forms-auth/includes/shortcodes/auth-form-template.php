8dae4354d2786dcece9f2910a8c6a7685600afe4
deyems <adeyemisola11@gmail.com>
2021-01-23 12:43:06 +0100

    template to be returned by shortcode

---
+++ plugins/forms-auth/includes/shortcodes/auth-form-template.php
@@ -0,0 +1,81 @@
+    <div class="col_one_third nobottommargin">
+
+        <div class="well well-lg nobottommargin">
+            <div id="login-status"></div>
+
+            <form id="login-form" class="nobottommargin">
+
+                <h3>Login to your Account</h3>
+
+                <div class="col_full">
+                    <label for="login-form-username">Username:</label>
+                    <input type="text" id="login-form-username" class="form-control" />
+                </div>
+
+                <div class="col_full">
+                    <label for="login-form-password">Password:</label>
+                    <input type="password" id="login-form-password" class="form-control" />
+                </div>
+
+                <div class="col_full nobottommargin">
+                    <button class="button button-3d nomargin" id="login-form-submit">Login</button>
+                </div>
+
+            </form>
+        </div>
+
+    </div>
+
+    <div class="col_two_third col_last nobottommargin">
+
+        <h3>Don't have an Account? Register Now.</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde,
+            vel odio non dicta provident sint ex autem mollitia dolorem illum
+            repellat ipsum aliquid illo similique sapiente fugiat minus ratione.
+        </p>
+
+        <div id="register-status"></div>
+
+        <form id="register-form" class="nobottommargin">
+
+            <div class="col_half">
+                <label for="register-form-name">Name:</label>
+                <input type="text" id="register-form-name" class="form-control"/>
+            </div>
+
+            <div class="col_half col_last">
+                <label for="register-form-email">Email Address:</label>
+                <input type="text" id="register-form-email" class="form-control"/>
+            </div>
+
+            <div class="clear"></div>
+
+            <div class="col_full">
+                <label for="register-form-username">Choose a Username:</label>
+                <input type="text" id="register-form-username" class="form-control" />
+            </div>
+
+            <div class="clear"></div>
+
+            <div class="col_half">
+                <label for="register-form-password">Choose Password:</label>
+                <input type="password" id="register-form-password" class="form-control" />
+            </div>
+
+            <div class="col_half col_last">
+                <label for="register-form-repassword">Re-enter Password:</label>
+                <input type="password" id="register-form-repassword" class="form-control" />
+            </div>
+
+            <div class="clear"></div>
+
+            <div class="col_full nobottommargin">
+                <button class="button button-3d button-black nomargin">
+                    Register Now
+                </button>
+            </div>
+
+        </form>
+
+    </div>
