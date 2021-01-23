6c533c923f8f28fe97761cb01b9f9a9c61aa6b20
deyems <adeyemisola11@gmail.com>
2021-01-23 10:21:18 +0100

    handling submission through AJAX on front-end

--- plugins/recipe/assets/js/main.js
+++ plugins/recipe/assets/js/main.js
@@ -1,4 +1,5 @@
 (function($){
+    //Handle Recipe ratings
     $("#recipe_rating").bind('rated', function(){
         $(this).rateit( 'readonly', true );
         var form = {
@@ -10,5 +11,35 @@
             
         });
         
+    });    
+    //Handling Form Submission from Front-end
+    $("#recipe-form").on( 'submit', function(e){
+        e.preventDefault();
+        //Hide the form during Submission
+        $(this).hide();
+        //
+        $("#recipe-status").html(`
+            <div class="alert alert-info">Please wait while we are submitting your recipe</div>
+        `);
+
+        let form = {
+            action: 'r_submit_user_recipe',
+            title: $("#r_inputTitle").val(),
+            content: tinymce.activeEditor.getContent()
+        }
+        // console.log(form);
+        $.post(recipe_obj.ajax_url, form, function(data){
+            console.log(data);
+            if(data.status == 2){
+                $("#recipe-status").html(`
+                    <div class="alert alert-success">Recipe submitted successfully!</div>
+                `);
+            }else{
+                $("#recipe-status").html(`
+                    <div class="alert alert-danger">Unable to submit recipe. Please fill in all fields </div>
+                `);
+                $("#recipe-form").show()
+            }
+        });
     });
 })(jQuery);
