de68d0b4007acd681756a5342350a937d2b981f0
deyems <adeyemisola11@gmail.com>
2021-01-23 22:20:37 +0100

    adding logic to hide register form when all user can register option is not checked and hide login form when user is logged in

--- plugins/forms-auth/includes/shortcodes/auth-form.php
+++ plugins/forms-auth/includes/shortcodes/auth-form.php
@@ -1,8 +1,15 @@
 <?php
 function my_myrecipe_auth_form_shortcode(){
+    if(is_user_logged_in()){
+        return '';
+    }
     $formHTML = file_get_contents('auth-form-template.php', true);
     $formHTML = str_replace('NONCE_FIELD_PH', 
-                            wp_nonce_field('my_myrecipe_auth','_wpnonce', true, false),
-                            $formHTML);
+                wp_nonce_field('my_myrecipe_auth','_wpnonce', true, false),
+                $formHTML);
+    $formHTML = str_replace('SHOW_REG_FORM',
+                (!get_option('users_can_register') ? 'style="display: none"' : ''),
+                $formHTML
+    );
     return $formHTML;
 }
