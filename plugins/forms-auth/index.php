58a0a201fe47bde355a97e535c4328a47fc52d2e
deyems <adeyemisola11@gmail.com>
2021-01-23 21:09:55 +0100

    add nopriv action for ajax and registration handling

--- plugins/forms-auth/index.php
+++ plugins/forms-auth/index.php
@@ -19,9 +19,13 @@ define('MYRECIPE_PLUGIN_URL', __FILE__);
 //Includes
 require_once __DIR__ . '/includes/shortcodes/auth-form.php';
 require_once __DIR__ . '/includes/front/enqueue.php';
+require_once __DIR__ . '/process/register-user.php';
+require_once __DIR__ . '/process/login-user.php';
 
 //Hooks
 add_action('wp_enqueue_scripts', 'my_enqueue_scripts', 100);
+add_action('wp_ajax_nopriv_my_register_user','my_register_user');
+add_action('wp_ajax_nopriv_my_login_user','my_login_user');
 
 //Shortcodes
 add_shortcode('myrecipe_auth_form', 'my_myrecipe_auth_form_shortcode');
