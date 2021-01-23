a7bc7f512bf1e6eaa75f052398d5688b691f4fa9
deyems <adeyemisola11@gmail.com>
2021-01-23 14:12:38 +0100

    replacing String content added to auth-form-template

--- plugins/forms-auth/includes/shortcodes/auth-form.php
+++ plugins/forms-auth/includes/shortcodes/auth-form.php
@@ -1,5 +1,8 @@
 <?php
 function my_myrecipe_auth_form_shortcode(){
     $formHTML = file_get_contents('auth-form-template.php', true);
+    $formHTML = str_replace('NONCE_FIELD_PH', 
+                            wp_nonce_field('my_myrecipe_auth','_wpnonce', true, false),
+                            $formHTML);
     return $formHTML;
 }
