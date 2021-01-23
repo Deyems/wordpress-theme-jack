5a8b5c380465042e9ed781f696dfe70882193bd4
deyems <adeyemisola11@gmail.com>
2021-01-23 03:15:14 +0100

    form template to input data

---
+++ plugins/recipe/includes/shortcodes/creator-template.php
@@ -0,0 +1,16 @@
+<form action="">
+    <div class="form-group">
+        <label for="">Title</label>
+        <input type="text" class="form-control" name="" id="r_inputTitle">
+    </div>
+    <!-- <div class="form-group">
+        <label for="">Content</label>
+        <textarea name="" id="recipecontenteditor" class="form-control"></textarea>
+    </div> -->
+    
+    CONTENT_EDITOR
+    
+    <div class="form-group">
+        <button type="submit" class="btn btn-primary">Submit Recipe</button>
+    </div>
+</form>
