2c974062a0d29b60502fe6e5151c44bd0e9b54d2
deyems <adeyemisola11@gmail.com>
2021-01-23 03:21:28 +0100

    adding id values to handle submission

--- plugins/recipe/includes/shortcodes/creator-template.php
+++ plugins/recipe/includes/shortcodes/creator-template.php
@@ -1,15 +1,10 @@
-<form action="">
+<div id="recipe-status"></div>
+<form id="recipe-form">
     <div class="form-group">
-        <label for="">Title</label>
+        <label for="r_inputTitle">Title</label>
         <input type="text" class="form-control" name="" id="r_inputTitle">
     </div>
-    <!-- <div class="form-group">
-        <label for="">Content</label>
-        <textarea name="" id="recipecontenteditor" class="form-control"></textarea>
-    </div> -->
-    
     CONTENT_EDITOR
-    
     <div class="form-group">
         <button type="submit" class="btn btn-primary">Submit Recipe</button>
     </div>
