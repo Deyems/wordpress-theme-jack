eca360d0ae052fe8a44aee28675ece6640f5df71
deyems <adeyemisola11@gmail.com>
2021-01-23 10:20:45 +0100

    template to inject to recipe to create recipe from frontend

--- plugins/recipe/includes/shortcodes/creator-template.php
+++ plugins/recipe/includes/shortcodes/creator-template.php
@@ -2,7 +2,7 @@
 <form id="recipe-form">
     <div class="form-group">
         <label for="r_inputTitle">Title</label>
-        <input type="text" class="form-control" name="" id="r_inputTitle">
+        <input type="text" class="form-control" name="title" id="r_inputTitle">
     </div>
     CONTENT_EDITOR
     <div class="form-group">
