53d26ec490ed6a1b1fdf2c86e50f3f0e41015d97
deyems <adeyemisola11@gmail.com>
2021-01-25 00:04:29 +0100

    adding prepare statement to one of our queries

--- plugins/recipe/process/filter-content.php
+++ plugins/recipe/process/filter-content.php
@@ -9,20 +9,17 @@ function r_filter_recipe_content($content){
     $user_IP = $_SERVER['REMOTE_ADDR'];
 
     $recipe_data = get_post_meta($post->ID, 'recipe_data', true);
-    // echo "<pre>";
-    // print_r($post->ID);
-    // print_r($recipe_data);
-    // print_r("At filter stage here");
-    // echo "</pre>";
     $recipe_html = file_get_contents('recipe-template.php', true);
     $recipe_html = str_replace('RATE_I18N:', __("Rating", 'udemy'), $recipe_html );
     $recipe_html = str_replace('RECIPE_ID', $post->ID, $recipe_html );
     $recipe_html = str_replace('RECIPE_RATING', $recipe_data['rating'], $recipe_html );
-
-    $rating_count = $wpdb->get_var(
+    
+    //Prepare Your Queries
+    $rating_count = $wpdb->get_var($wpdb->prepare(
         "SELECT COUNT(*) FROM `". $table_name. "` 
-        WHERE recipe_id='". $post->ID ."' AND user_ip= '". $user_IP. "'"
-    );
+        WHERE recipe_id=%d AND user_ip=%s",
+        $post->ID, $user_IP
+    ));
 
     if($rating_count > 0 ){
         $recipe_html = str_replace('READONLY_PLACEHOLDER', 'data-rateit-READONLY="true"', $recipe_html);
@@ -30,5 +27,5 @@ function r_filter_recipe_content($content){
         $recipe_html = str_replace('READONLY_PLACEHOLDER', '', $recipe_html);
     }
 
-    return "$recipe_html" . $content;
+    return $recipe_html . $content;
 }
