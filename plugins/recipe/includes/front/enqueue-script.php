9a249c5d2c647321517c4896542e8fa8b1ae510f
deyems <adeyemisola11@gmail.com>
2021-01-24 22:51:07 +0100

    register and enqueue auth css

--- plugins/recipe/includes/front/enqueue-script.php
+++ plugins/recipe/includes/front/enqueue-script.php
@@ -3,12 +3,17 @@
 function r_enqueue_scripts(){
     wp_register_style('r_rateit', 
     plugins_url('/assets/rateit/rateit.css', RECIPE_PLUGIN_URL));
-
-    wp_register_style('ju_bootstrap', 
+    
+    //Bootstrap added just for when shipping to another platform
+    wp_register_style('r_bootstrap', 
     plugins_url('/assets/css/bootstrap.css', RECIPE_PLUGIN_URL));
+    
+    wp_register_style('r_auth', 
+    plugins_url('/assets/css/auth.css', RECIPE_PLUGIN_URL));
 
     wp_enqueue_style('r_rateit');
-    wp_enqueue_style('ju_bootstrap');
+    wp_enqueue_style('r_bootstrap');
+    wp_enqueue_style('r_auth');
     
     wp_register_script('r_rate_it', 
     plugins_url('/assets/rateit/jquery.rateit.min.js', RECIPE_PLUGIN_URL),
@@ -24,7 +29,6 @@ function r_enqueue_scripts(){
 
     wp_localize_script('r_main', 'recipe_obj', [
         'ajax_url' => admin_url('admin-ajax.php'),
-        'nonce' => wp_create_nonce('wp_rest'),
     ]);
 
     wp_enqueue_script('r_rate_it');
