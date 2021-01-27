2b708252ff46783cf55b93a306a72cfd9ee87027
deyems <adeyemisola11@gmail.com>
2021-01-27 18:29:33 +0100

    admin_post_action hook to handle options api saving

--- plugins/recipe/includes/admin/init.php
+++ plugins/recipe/includes/admin/init.php
@@ -8,4 +8,5 @@ function recipe_admin_init(){
     add_filter('manage_recipes_posts_columns', 'r_add_new_recipe_columns');
     add_action('manage_recipes_posts_custom_columns', 'r_manage_recipe_columns', 10, 2);
     add_action('admin_enqueue_scripts', 'r_admin_enqueue');
+    add_action('admin_post_r_save_options', 'r_save_options');
 }
