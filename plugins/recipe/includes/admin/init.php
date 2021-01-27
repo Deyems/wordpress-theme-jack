a76e2e424992a284f977108f7fa31d0be2fbcdcf
deyems <adeyemisola11@gmail.com>
2021-01-27 12:43:54 +0100

    enqueue scripts to admin

--- plugins/recipe/includes/admin/init.php
+++ plugins/recipe/includes/admin/init.php
@@ -4,6 +4,8 @@
 
 function recipe_admin_init(){
     require_once __DIR__ . '/column.php';
+    require_once __DIR__ . '/enqueue.php';
     add_filter('manage_recipes_posts_columns', 'r_add_new_recipe_columns');
     add_action('manage_recipes_posts_custom_columns', 'r_manage_recipe_columns', 10, 2);
+    add_action('admin_enqueue_scripts', 'r_admin_enqueue');
 }
