337d61c20b2533aa991217284d73d4432d57243d
deyems <adeyemisola11@gmail.com>
2021-01-30 11:08:12 +0100

    adding condition to stop unnecessary submission or creation of recipe

--- plugins/recipe/includes/shortcodes/creator.php
+++ plugins/recipe/includes/shortcodes/creator.php
@@ -1,6 +1,10 @@
 <?php
 
 function r_recipe_creator_shortcode(){
+    $recipe_options = get_option('r_opts');
+    if(!is_user_logged_in() && $recipe_options['recipe_submission_login_required'] == 2){
+        return 'You must be logged in to submit a recipe';
+    }
     //Converting file_get_contents to wp_remote_get function
     $creator_tpl_res = wp_remote_get(
         plugins_url('includes/shortcodes/creator-template.php', RECIPE_PLUGIN_URL)
