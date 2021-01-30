1a033671061d6dadf1a11a53cf9eee382640a02c
deyems <adeyemisola11@gmail.com>
2021-01-30 11:41:25 +0100

    added tags to help handle upload of media

--- plugins/recipe/includes/shortcodes/creator-template.php
+++ plugins/recipe/includes/shortcodes/creator-template.php
@@ -5,6 +5,15 @@
         <input type="text" class="form-control" name="title" id="r_inputTitle">
     </div>
     CONTENT_EDITOR
+    <hr>
+    <div class="form-group">
+        <label for="">Featured Image 
+            <a href="#" id="recipe-img-upload-btn">Upload</a>
+        </label>
+        <br>
+        <img src="" alt="" id="recipe-img-preview">
+        <input type="hidden" name="" id="r_inputImgID">
+    </div>
     <div class="form-group">
         <button type="submit" class="btn btn-primary">Submit Recipe</button>
     </div>
