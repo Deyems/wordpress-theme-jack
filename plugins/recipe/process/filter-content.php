bdccfa36650aa2e2b78b7dc0f0582deedef0ab2a
deyems <adeyemisola11@gmail.com>
2021-02-02 01:42:59 +0100

    filter template strings

--- plugins/recipe/process/filter-content.php
+++ plugins/recipe/process/filter-content.php
@@ -13,9 +13,21 @@ function r_filter_recipe_content($content){
         plugins_url('process/recipe-template.php', RECIPE_PLUGIN_URL)
     );
     $recipe_html = wp_remote_retrieve_body($recipe_tpl_res);
+    $origin = wp_get_post_terms($post->ID, 'origin');
+    // var_dump($origin);
+    $more_info_url = isset($origin[0]) ? get_term_meta($origin[0]->term_id, 'more_info_url', true) : '';
     $recipe_html = str_replace('RATE_I18N:', __("Rating", 'udemy'), $recipe_html );
     $recipe_html = str_replace('RECIPE_ID', $post->ID, $recipe_html );
     $recipe_html = str_replace('RECIPE_RATING', $recipe_data['rating'], $recipe_html );
+
+    $recipe_html = str_replace('ORIGIN_118N', __('Origin','recipe'), $recipe_html);
+    $recipe_html = str_replace('ORIGIN_PH', isset($origin[0]) ? $origin[0]->name : 'None', $recipe_html);
+
+    if(!empty($more_info_url)){
+        $recipe_html = str_replace("_MORE_INFO_URL_PH", '<a href="'. $more_info_url .'"> More info </a>',$recipe_html);
+    }else{
+        $recipe_html = str_replace("_MORE_INFO_URL_PH", '', $recipe_html);
+    }
     
     //Prepare Your Queries
     $rating_count = $wpdb->get_var($wpdb->prepare(
