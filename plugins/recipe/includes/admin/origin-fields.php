8c68d265fce3b6802fa2d02bbfc3f53a859cfd99
deyems <adeyemisola11@gmail.com>
2021-02-02 01:43:30 +0100

    field to show at admin for taxonomy created

--- plugins/recipe/includes/admin/origin-fields.php
+++ plugins/recipe/includes/admin/origin-fields.php
@@ -3,8 +3,8 @@
 function r_origin_add_form_fields(){
     ?>
     <div class="form-field">
-        <label for=""><?php _e('More info url', 'recipe') ?></label>
-        <input type="text" name="r_more_info_url">
+        <label for="r_more_info_url"><?php _e('More info url', 'recipe') ?></label>
+        <input type="text" name="r_more_info_url" id="r_more_info_url">
         <p class="description">
             <?php esc_html_e('A url a user can click to learn more about this origin', 'recipe') ?>
         </p>
