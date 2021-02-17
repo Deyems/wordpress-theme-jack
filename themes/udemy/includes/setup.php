a94f649a981d66b9b574ed168cce1d5019c20c7c
deyems <adeyemisola11@gmail.com>
2021-02-17 14:00:35 +0100

    add_theme_support

--- themes/udemy/includes/setup.php
+++ themes/udemy/includes/setup.php
@@ -101,6 +101,7 @@ function ju_setup_theme(){
     
 
     ]);
+    add_theme_support('woocommerce');
     register_nav_menu( 'primary', __( 'Primary Menu', 'udemy' ) );
     register_nav_menu( 'secondary', __( 'Secondary Menu', 'udemy' ) );
 
